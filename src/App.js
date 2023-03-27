ca63b8092563e81509a9224f660ec0b471f8f99f
Sri Ram Kakarla <tapukoko9611@gmail.com>
2023-03-27 21:50:12 +0530

    my changes

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
           <Route path='/' element={ <Home /> } />
           <Route path="/ayo/adda/:addaId" element={ <Adda socket={socket}/> } />
           <Route path="/ayo/storage/:storageId" element={ <Storage socket={socket}/> } />
-          <Route path="/ayo/ign/:query" element={ <Incognito /> } />
+          <Route path="ayo/ign/:query" element={ <Incognito /> } />
         </Routes>
       </AuthContextProvider>
     </div>

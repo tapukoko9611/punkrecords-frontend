34ed4982c52de93b97768ef2901e6d5f54b5389d
Sri Ram Kakarla <tapukoko9611@gmail.com>
2023-03-27 21:36:50 +0530

    my changes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename='https://punkrecord.onrender.com'>
       {/* <React.StrictMode> */}
         <App />
       {/* </React.StrictMode> */}

387d33ac8d4fe04fd7c4a9980deaeee0c28937b5
Sri Ram Kakarla <tapukoko9611@gmail.com>
2023-03-29 00:00:27 +0530

    my changes

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
     <div className="App" >
       <AuthContextProvider> 
         <Routes>
-          <Route path="/" element={ <Incognito /> } />
+          <Route path="/" element={ <Incognito /> } exact />
           {/* <Route path='/' element={ <Home /> } /> */}
           <Route path="/ayo/adda/:addaId/" element={ <Adda socket={socket}/> } />
           <Route path="/ayo/storage/:storageId/" element={ <Storage socket={socket}/> } />

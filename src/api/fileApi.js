9022f075f8bca060e9a533c93ab3aedc11fd744b
tapukoko9611 <tapukoko9611@gmail.com>
2025-05-24 19:48:52 +0530

    trying bruh

--- src/api/fileApi.js
+++ src/api/fileApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = '/api/files';
+const API_BASE_URL = (process.env.API_BASE_URL|| "") + '/api/files';
 
 const fileApi = {
     checkFileName: async (token, fileName) => {
--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = '/api/users';
+const API_BASE_URL = (process.env.API_BASE_URL|| "") + '/api/users';
 
 const authApi = {
     login: async (userName, password) => {
